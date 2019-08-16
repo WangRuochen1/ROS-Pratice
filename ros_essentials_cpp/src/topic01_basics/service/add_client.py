980e8d29f23dfad69d75cb9ae426cab9878f6323
WangRuochen1 <wruochen1999@hotmail.com>
2019-08-16 11:01:59 +0800

    ros_service_py_cpp

--- ros_essentials_cpp/src/topic01_basics/service/add_client.py
+++ ros_essentials_cpp/src/topic01_basics/service/add_client.py
@@ -6,10 +6,11 @@ from ros_essentials_cpp.srv import AddTwoInts
 from ros_essentials_cpp.srv import AddTwoIntsRequest
 from ros_essentials_cpp.srv import AddTwoIntsResponse
 
-def add_two_ints_client(x, y):
+def add_two_ints_client(x, y): 
     rospy.wait_for_service('add_two_ints')
     try:
         add_two_ints = rospy.ServiceProxy('add_two_ints', AddTwoInts)
+        # ServiceProxy is responsible for sending the request
         resp1 = add_two_ints(x, y)
         return resp1.sum
     except rospy.ServiceException, e:
@@ -20,11 +21,11 @@ def usage():
 
 if __name__ == "__main__":
     if len(sys.argv) == 3:
-        x = int(sys.argv[1])
+        x = int(sys.argv[1])# read x and y value from the arguments
         y = int(sys.argv[2])
     else:
         print usage()
         sys.exit(1)
     print "Requesting %s+%s"%(x, y)
-    s = add_two_ints_client(x, y)
+    s = add_two_ints_client(x, y)#this function is used to formulate the request and send it
     print "%s + %s = %s"%(x, y, s)
--- ros_essentials_cpp/src/topic01_basics/service/add_server.py
+++ ros_essentials_cpp/src/topic01_basics/service/add_server.py
@@ -9,10 +9,15 @@ import rospy
 def handle_add_two_ints(req):
     print "Returning [%s + %s = %s]"%(req.a, req.b, (req.a + req.b))
     return AddTwoIntsResponse(req.a + req.b)
+    #return to response
 
 def add_two_ints_server():
-    rospy.init_node('add_two_ints_server')
+    rospy.init_node('add_two_ints_server')#initialize the node
     s = rospy.Service('add_two_ints', AddTwoInts, handle_add_two_ints)
+    #create a server listen to incoming request 
+    #'add_two_ints' is the name of the service
+    #AddTwoInts is the type of message
+    #handle_add_two_ints is the function be called everytime a new request comming
     print "Ready to add two ints."
     rospy.spin()
     
